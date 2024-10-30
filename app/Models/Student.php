6f49ce238119b5acd5f7eb4f1d1061c108195184
rezalastana <rezalastana.dev@gmail.com>
2024-10-30 08:34:08 +0700

    feat: fill the models

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nis','name','gender','birthday','religion','contact','profile'];
 }
--- app/Models/Subject.php
+++ app/Models/Subject.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Subject extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['kode','name','slug'];
 }
